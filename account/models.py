5ee9568439037ab3ea09d041c2c5014a2b8f1e13
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-11-04 13:40:15 +0100

    added phone number field

--- account/models.py
+++ account/models.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=12,blank=True,null=True)
     profile = models.ImageField(upload_to="profiles", blank=True, null=True)
